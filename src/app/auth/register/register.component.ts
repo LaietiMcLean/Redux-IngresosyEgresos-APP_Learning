cf807816138f3b1781cee088b462a743cb1f6788
Laiamdelprado <lmarcodelprado@gmail.com>
2022-12-26 20:36:28 +0100

    feat(): form group

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.component.html'
+  templateUrl: './register.component.html',
 })
 export class RegisterComponent implements OnInit {
+  formRegister!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formRegister = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
+  createUser() {
+    console.log(this.formRegister.value);
+  }
 }
