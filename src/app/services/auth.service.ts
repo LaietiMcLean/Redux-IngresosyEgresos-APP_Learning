aca968dc080f0d9c8c9bdabad26179079ffc33aa
Laiamdelprado <lmarcodelprado@gmail.com>
2023-02-19 15:28:05 +0100

    feat(auth.service): get active user info

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,6 +8,14 @@ export class AuthService {
 
   constructor(public auth: AngularFireAuth) { }
 
+  initAuthListener() {
+    this.auth.authState.subscribe(firebaseUser => {
+      console.log(firebaseUser);
+      console.log(firebaseUser?.email);
+      console.log(firebaseUser?.uid);
+    })
+  }
+
   createUser(nombre: string, correo: string, password: string) {
     return this.auth.createUserWithEmailAndPassword(correo, password);
   }
