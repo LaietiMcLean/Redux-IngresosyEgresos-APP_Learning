d58b0890577cb1743674e1d51d12fab3a017681b
Laiamdelprado <lmarcodelprado@gmail.com>
2023-02-19 15:40:47 +0100

    feat(auth.guard): add guard - route protection if user is not active

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(public auth: AngularFireAuth) { }
+  constructor(public auth: AngularFireAuth) {}
 
   initAuthListener() {
-    this.auth.authState.subscribe(firebaseUser => {
+    this.auth.authState.subscribe((firebaseUser) => {
       console.log(firebaseUser);
       console.log(firebaseUser?.email);
       console.log(firebaseUser?.uid);
-    })
+    });
   }
 
   createUser(nombre: string, correo: string, password: string) {
@@ -25,6 +25,12 @@ export class AuthService {
   }
 
   logout() {
-   return  this.auth.signOut();
+    return this.auth.signOut();
+  }
+
+  isAuth() {
+    return this.auth.authState.pipe(
+      map((firebaseUser) => firebaseUser !== null)
+    );
   }
 }
